d54aa979a77ff2a9e2201fcba88df03fe41ed5c2
jesperskogman <jesper.skogman@elev.ga.ntig.se>
2020-02-25 10:19:25 +0100

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class Warior {
+    private int HP;
+    private int maxHP;
+    private int xp;
+    private int atk;
+    private int def;
+    private int lvl;
+
+    private int numPotions;
+
+    public Warior(){
+        HP = maxHP;
+        lvl = 1;
+        def = 5;
+        atk = 4;
+    }
+}
