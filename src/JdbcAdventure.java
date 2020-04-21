208b11e901bf57668dad134805d5a688e2199b79
jesperskogman <jesper.skogman@elev.ga.ntig.se>
2020-04-21 10:42:36 +0200

    Lade till en javaclass för GUI där interfacen ska göras

--- src/JdbcAdventure.java
+++ src/JdbcAdventure.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.util.Scanner;
 
-public class JdbcSelectTest {
+public class JdbcAdventure {
     public static void main(String[] args) {
         try (
                 Connection conn = DriverManager.getConnection(
