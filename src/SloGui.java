5492d4bcd65338bfa8646c846059a7d2dfb2e9f7
jesperskogman <jesper.skogman@elev.ga.ntig.se>
2020-04-28 09:39:14 +0200

    Har gjort en primär design på mitt GUI så att jag ska kunna ha något att stoppa in datan i

--- src/SloGui.java
+++ src/SloGui.java
@@ -3,6 +3,11 @@ import javax.swing.*;
 public class SloGui {
 
     private JPanel panel1;
+    private JTextArea textArea1;
+
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("SloGui");
